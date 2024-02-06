7ac90f6a684ac2c01879bb467113c51550094db4
rk1942 <rishabhkarthikeya@gmail.com>
2024-02-06 11:29:14 +0530

    changes

--- score.js
+++ score.js
@@ -1,1 +1,10 @@
 // Iteration 8: Making scoreboard functional
+let score = localStorage.getItem("score");
+var scoreboard = document.getAnimations("score-board");
+const playAgain = document.getElementById("play-again-button");
+
+scoreboard.innerHtml = score;
+
+playAgain.onclick = () =>{
+    location.href = "game.html"
+};
